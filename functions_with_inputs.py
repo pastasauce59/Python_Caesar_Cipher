510ca2ec145a8c0443b41a755598e278ce855292
Michael Primavera <pastasauce59@gmail.com>
2023-02-12 23:25:07 -0500

    initial commit

---
+++ functions_with_inputs.py
@@ -0,0 +1,21 @@
+# PYTHON FUNCTIONS WITH INPUTS / ARGUMENTS / PARAMETERS
+
+# def my_function(a, b , c):
+    # DO THIS WITH a
+    # THEN DO THIS WITH b
+    # FINALLY DO THIS WITH c
+
+
+#POSITIONAL ARGUMENTS VS. KEYWORD ARGUMENTS
+
+#POSITIONAL ARGUMENTS WILL CALLED IN THE ORDER THEY ARE PLACED WHILE KEYWORD ARGUMENTS ARE PARAMETERS
+#ADDED WITH THE ARGUMENT KEYWORD AND CAN BE CALLED IN ANY ORDER.
+
+#EXAMPLE
+
+def my_function(a, b, c):
+    print(f"This is {a}")
+    print(f"Then this is {b}")
+    print(f"Finally this is {c}")
+
+my_function(c=2, a=1, b=3)
