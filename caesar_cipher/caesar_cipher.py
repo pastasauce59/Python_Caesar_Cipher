a08ff6b494b6062f9c53cf2c4a5392a5b8b495bb
Michael Primavera <pastasauce59@gmail.com>
2023-02-14 23:12:30 -0500

    added logic to ask the user if they would like to restart the program and continue encoding/decoding messages with boolean and if statement.

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,25 +1,32 @@
 from art import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+restart = True
 print(logo)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+while restart:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
-if direction == "encode" or direction == "decode":
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
+    if direction == "encode" or direction == "decode":
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
 
-    def caesar(text, shift, direction):
-        cipher_text = ""
-        for letter in text:
-            if direction.lower() == "encode":
-                shift_num = alphabet.index(letter) + shift
-                cipher_text += alphabet[shift_num]
-            elif direction.lower() == "decode":
-                shift_num = alphabet.index(letter) - shift
-                cipher_text += alphabet[shift_num]
-        print(f"Your {direction}d message is: {cipher_text}")
+        def caesar(text, shift, direction):
+            cipher_text = ""
+            for letter in text:
+                if direction.lower() == "encode":
+                    shift_num = alphabet.index(letter) + shift
+                    cipher_text += alphabet[shift_num]
+                elif direction.lower() == "decode":
+                    shift_num = alphabet.index(letter) - shift
+                    cipher_text += alphabet[shift_num]
+            print(f"Your {direction}d message is: {cipher_text}")
 
-    caesar(text, shift, direction)
-else:
-    print("Please select \"encode\" or \"decode\".")
+        caesar(text, shift, direction)
+
+        continue_ = input("Do you want to continue ciphering/deciphering? Type \"Yes\" or \"No\" \n")
+        if continue_.lower() != "yes":
+            restart = False
+        
+    else:
+        print("Please select \"encode\" or \"decode\".")
